373fa995cac0df4e6909d73a5734019da2b904c1
Milene <79708958+Mileneapm@users.noreply.github.com>
2021-08-04 22:52:33 -0300

    Aplicacao exercicio final, estrutura

--- appbd/router/routing.js
+++
@@ -1,7 +0,0 @@
-const routing = require('express').Router();
-
-var motoristaController = require('../controller/motorista')
-var veiculoController = require('../controller/veiculo')
-
-  app.use(motoristaController)
-  app.use(veiculoController)
